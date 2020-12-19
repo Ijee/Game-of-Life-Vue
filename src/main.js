ee9b484893d6ba44b58d846eeab65a5aa877fa62
Thorsten Schulz <thorsten.schulz2@hansemerkur.de>
2020-12-19 12:19:03 +0100

    fix icons not being shown

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 
 library.add(fas)
 library.add(fab)
+library.add(far)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 
